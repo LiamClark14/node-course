f23c5cd429a5272a1d9032bb677998c79f23ef46
LiamClark14 <liam.clark@duke.edu>
2021-07-18 19:57:51 -0400

    initial commit

---
+++ sec-2-js-syntax/play.js
@@ -0,0 +1,16 @@
+var name = 'Liam';
+var age = 20;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby){
+    return (
+        'Name is ' +
+        userName +
+        ', age is ' +
+        userAge +
+        ' and the user has hobbies: ' +
+        userHasHobby
+    );
+}
+
+console.log(summarizeUser(name,age,hasHobbies));
