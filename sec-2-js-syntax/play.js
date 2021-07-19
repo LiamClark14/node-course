9a18496502a8e91fa1df486231c1cbfa38f69ece
LiamClark14 <liam.clark@duke.edu>
2021-07-19 15:15:06 -0400

    Working through section 2

--- sec-2-js-syntax/play.js
+++ sec-2-js-syntax/play.js
@@ -1,16 +1,41 @@
-var name = 'Liam';
-var age = 20;
-var hasHobbies = true;
+// const name = 'Liam';
+// let age = 20;
+// const hasHobbies = true;
 
-function summarizeUser(userName, userAge, userHasHobby){
-    return (
-        'Name is ' +
-        userName +
-        ', age is ' +
-        userAge +
-        ' and the user has hobbies: ' +
-        userHasHobby
-    );
-}
+// age = 21;
 
-console.log(summarizeUser(name,age,hasHobbies));
+// function summarizeUser(userName, userAge, userHasHobby){
+//     return (
+//         'Name is ' +
+//         userName +
+//         ', age is ' +
+//         userAge +
+//         ' and the user has hobbies: ' +
+//         userHasHobby
+//     );
+// }
+
+// //const add = (a,b) => a + b;
+
+// //const addOne = (a) => a + 1;
+
+
+// console.log(summarizeUser(name,age,hasHobbies));
+
+
+// a key-value pair is also called a "property" or a "field" of the object
+const person = {
+    name: 'Liam',
+    age: 20,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+const hobbies = ['Sports', 'Jiu Jitsu', 'Cooking'];
+
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+hobbies.push('Programming');
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
